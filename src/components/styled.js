add27d9fbb738b8251cc9195ca2cc42d0444a5e0
ankit-sengar <sengar.ar46@gmail.com>
2021-05-15 15:48:04 +0530

    Creating Style Part 4

--- src/components/styled.js
+++ src/components/styled.js
@@ -5,6 +5,7 @@ export const FlexGrid = styled.div`
   justify-content: center;
   flex-wrap: wrap;
 `;
+
 export const SearchCard = styled.div`
   width: 300px;
   height: 100%;
@@ -32,4 +33,22 @@ export const SearchCard = styled.div`
   p {
     margin: 0;
   }
+`;
+export const Star = styled.div`
+  display: inline-block;
+  width: 18px;
+  height: 18px;
+  background-color: #ffc806;
+  clip-path: polygon(
+    50% 0%,
+    61% 35%,
+    98% 35%,
+    68% 57%,
+    79% 91%,
+    50% 70%,
+    21% 91%,
+    32% 57%,
+    2% 35%,
+    39% 35%
+  );
 `;
