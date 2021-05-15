bd2884102bb74ea9dbc1eea227642bac8f48d9e3
ankit-sengar <sengar.ar46@gmail.com>
2021-05-15 13:42:59 +0530

    adding load and error messages

---
+++ src/pages/Show.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiGet } from '../misc/config';
+
+const Show = () => {
+  const { id } = useParams();
+
+  const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    apiGet(`/shows/1?embed[]=episodes&embed[]=cast`)
+      .then(results => {
+        setTimeout(() => {
+          if (isMounted) {
+            setShow(results);
+            setIsLoading(false);
+          }
+        }, 2000);
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
+  }, [id]);
+  // eslint-disable-next-line no-console
+  console.log('show', show);
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+  if (error) {
+    return <div>Error occured:{error}</div>;
+  }
+  return <div>this is a show page</div>;
+};
+
+export default Show;
